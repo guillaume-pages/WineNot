83d18963e973d3a8fd242ce1698123d7ed8143ec
guillaume-pages <guillaumepages@outlook.com>
2024-04-06 18:24:14 +0200

    modif visuel logo navbar mobile

--- app/ui/navbar/navbar-mobile.tsx
+++ app/ui/navbar/navbar-mobile.tsx
@@ -7,14 +7,14 @@ import { PiChartLineUpThin } from "react-icons/pi";
 import { CiLogin } from "react-icons/ci";
 import { CiLogout } from "react-icons/ci";
 import { CiSettings } from "react-icons/ci";
-import LogoMoyen from '@/app/logo-moyen.png'
+import LogoPetit from '@/app/logo-petit.png'
 
 export default function NavbarMobile() {
   const isConnected = false;
   return (
     <>
       <div className="md:hidden flex w-full space-x-8 justify-center fixed bottom-0">
-        <Image src={LogoMoyen} height={28} width={28} alt="Compagny logo" />
+        <Image src={LogoPetit} height={36} width={36} alt="Compagny logo" />
         <PiWineThin size={28} />
         <PiHouseLineLight size={30} />
         <PiChartLineUpThin size={32} />
