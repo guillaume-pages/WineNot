af79a2be370bd79ba0faa89678e6953e475cd612
guillaume-pages <guillaumepages@outlook.com>
2024-04-06 18:15:29 +0200

    test size icon on real phone

--- app/ui/navbar/navbar-mobile.tsx
+++ app/ui/navbar/navbar-mobile.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from "next/image";
 
 import { PiWineThin } from "react-icons/pi";
 import { PiHouseLineLight } from "react-icons/pi";
@@ -6,17 +7,19 @@ import { PiChartLineUpThin } from "react-icons/pi";
 import { CiLogin } from "react-icons/ci";
 import { CiLogout } from "react-icons/ci";
 import { CiSettings } from "react-icons/ci";
+import LogoMoyen from '@/app/logo-moyen.png'
 
 export default function NavbarMobile() {
   const isConnected = false;
   return (
     <>
       <div className="md:hidden flex w-full space-x-8 justify-center fixed bottom-0">
+        <Image src={LogoMoyen} height={28} width={28} alt="Compagny logo" />
         <PiWineThin size={28} />
-        <PiHouseLineLight size={28} />
-        <PiChartLineUpThin size={28} />
-        {isConnected ? <CiLogout size={28} /> : <CiLogin size={28} />}
-        <CiSettings size={28} />
+        <PiHouseLineLight size={30} />
+        <PiChartLineUpThin size={32} />
+        {isConnected ? <CiLogout size={34} /> : <CiLogin size={34} />}
+        <CiSettings size={36} />
       </div>
     </>
   )
