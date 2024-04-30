8b9d69e2ac3ae2cee94745900ada6304adfb8657
guillaume-pages <guillaumepages@outlook.com>
2024-04-30 16:15:34 +0200

    back button for register and login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,12 +8,14 @@ import logoPetit from '@/app/logo-petit.png';
 import { LoginForm } from './login-form';
 import { Suspense } from 'react';
 
+import { FaArrowLeftLong } from 'react-icons/fa6';
+
 export default function LoginPage() {
   return (
     <>
       <section>
         <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-          <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <section className="relative flex h-32 items-end lg:col-span-5 lg:h-full xl:col-span-6">
             <Image
               alt="Des bouteilles de vin - Photo de Markus Spiske sur Unsplash"
               src={bottles}
@@ -22,12 +24,14 @@ export default function LoginPage() {
               className="absolute inset-0 h-full w-full object-cover opacity-80"
             ></Image>
           </section>
-
           <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+            <Link href="/" className="absolute left-4 top-4">
+              <FaArrowLeftLong className="text-3xl text-slate-200" />
+            </Link>
             <div className="max-w-xl lg:max-w-3xl">
-              <div className="relative -mt-16 block">
+              <div className="relative -mt-16 block pb-4">
                 <Link
-                  className="size-16 sm:size-20 inline-flex items-center justify-center rounded-full bg-white text-blue-600"
+                  className="size-16 sm:size-20 inline-flex items-center justify-center rounded-full"
                   href="/"
                 >
                   <Image
@@ -37,11 +41,11 @@ export default function LoginPage() {
                   ></Image>
                 </Link>
 
-                <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                <h1 className="mt-2 text-2xl font-bold sm:text-3xl md:text-4xl">
                   Bon retour sur Cavavin !
                 </h1>
 
-                <p className="mt-4 leading-relaxed text-gray-500">
+                <p className="mt-4 leading-relaxed">
                   Heureux de vous revoir, reprenez où vous en étiez !
                 </p>
               </div>
