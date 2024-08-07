c401aabd9d892fa8d56266b25d582e279db4c19d
guillaume-pages <guillaumepages@outlook.com>
2024-08-07 12:48:45 +0200

    ajout de message pour l'update bouteille, ajout de message pour la création bouteille

--- app/lib/bottle/bottle.put.ts
+++ app/lib/bottle/bottle.put.ts
@@ -9,23 +9,23 @@ const prisma = new PrismaClient();
 const UpdateBottleSchema = z.object({
   bottle_name: z.string({
     required_error: 'Le nom de la bouteille est requis.',
-  }).min(3),
-  millesime: z.number({ required_error: 'Le millesime est requis.' }),
-  type_of_wine: z.string({ required_error: 'Le type de vin est requis.' }),
+  }).min(1, { message: 'Le nom de la bouteille ne doit pas être vide.' }),
+  millesime: z.number({ required_error: 'Le millesime est requis.' }).min(3),
+  type_of_wine: z.string({ required_error: 'Le type de vin est requis.' }).min(1, { message: 'Le type de vin est requis.' }),
   size: z.string({ required_error: 'La taille de la bouteille est requise.' }),
   grape_varieties: z.array(z.string()).optional(),
   region: z.string({ required_error: 'La région est requise.' }),
-  eye_description: z.string().max(300).optional(),
+  eye_description: z.string().max(500, { message: 'La description doit comporter au maximum 500 caractères.' }).optional(),
   nose_description: z.array(z.string()).optional(),
   mouth_description: z.array(z.number()).optional(),
   carafage: z.number().optional(),
   temperature: z.number().optional(),
-  degree: z.number({ required_error: "Le degré d'alcool est requis"}),
+  degree: z.number({ required_error: "Le degré d'alcool est requis" }),
   accompaniment: z.array(z.string()).optional(),
   media: z.string().optional(),
   price: z.number({ required_error: 'Le prix est requis.' }),
   price_visibility: z.number({ required_error: 'La visibilité du prix est requise.' }),
-  global_description: z.string().max(400).optional(),
+  global_description: z.string().max(400, { message: 'La description doit comporter au maximum 400 caractères.' }).optional(),
   entry_date: z.date({ required_error: "La date d'entrée est requise." }),
   potential_date: z.date().optional(),
   quantity: z.number({ required_error: 'La quantité est requise.' }),
@@ -58,9 +58,12 @@ export const updateBottle = async (data: any) => {
       message: 'Bouteille modifiée avec succès.',
     };
   } catch (error) {
-    console.error(error);
-    throw new Error('Il y a un problème avec les champs du formulaire. Veuillez vérifier et réessayer.');
-  } finally {
+    if (error instanceof z.ZodError) {
+      const errorMessages = error.errors.map(err => err.message).join(' ');
+      return { errors: errorMessages };
+    } else {
+      return { errors: 'Une erreur inattendue est survenue. Veuillez réessayer.' };
+    }  } finally {
     await prisma.$disconnect();
   }
 };
