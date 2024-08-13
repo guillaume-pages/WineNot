f1add81faf78d3e0e4d74d8d43d9e1b89fd9538f
guillaume-pages <guillaumepages@outlook.com>
2024-08-13 13:53:30 +0200

    ajout d'une migration pour enlever des tables inutilisées

---
+++ prisma/migrations/20240813115152_remove_unused_tables/migration.sql
@@ -0,0 +1,37 @@
+/*
+  Warnings:
+
+  - You are about to drop the `accounts` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `customers` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `invoices` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `revenue` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `sessions` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `verification_request` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "accounts" DROP CONSTRAINT "accounts_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "invoices" DROP CONSTRAINT "invoices_customer_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "sessions" DROP CONSTRAINT "sessions_user_id_fkey";
+
+-- DropTable
+DROP TABLE "accounts";
+
+-- DropTable
+DROP TABLE "customers";
+
+-- DropTable
+DROP TABLE "invoices";
+
+-- DropTable
+DROP TABLE "revenue";
+
+-- DropTable
+DROP TABLE "sessions";
+
+-- DropTable
+DROP TABLE "verification_request";
