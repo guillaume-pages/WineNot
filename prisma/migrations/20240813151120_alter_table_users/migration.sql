7625393d45c19e919cae55955eb4db9902c810a5
guillaume-pages <guillaumepages@outlook.com>
2024-08-13 17:11:54 +0200

    alter table users

---
+++ prisma/migrations/20240813151120_alter_table_users/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `users` table. All the data in the column will be lost.
+  - The `email_verified` column on the `users` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3),
+DROP COLUMN "email_verified",
+ADD COLUMN     "email_verified" BOOLEAN NOT NULL DEFAULT false;
