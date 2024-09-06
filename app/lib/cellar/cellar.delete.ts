69f16da8a6c5e11a571c0f17b4ddf6a79a205a63
guillaume-pages <guillaumepages@outlook.com>
2024-09-06 11:50:53 +0200

    ajout suppression de cellarId dans bottles_cellars lors d'une suppression de cave

--- app/lib/cellar/cellar.delete.ts
+++ app/lib/cellar/cellar.delete.ts
@@ -6,6 +6,12 @@ import { revalidatePath } from 'next/cache';
 
 export const deleteCellar = async (cellarId: string) => {
   try {
+    await prisma.bottles_cellars.deleteMany({
+      where: {
+        cellar_id: cellarId,
+      },
+    });
+    
     await prisma.users_cellars.deleteMany({
       where: {
         cellar_id: cellarId,
@@ -18,12 +24,15 @@ export const deleteCellar = async (cellarId: string) => {
       },
     });
 
+    console.log('Cellar deleted');
+
     revalidatePath('/cellar');
 
     return {
       message: 'La cave a été supprimée avec succès.',
     };
   } catch (error) {
+    console.error('Error while deleting cellar', error);
     throw new Error(
       'Erreur lors de la suppression de la cave. Veuillez réessayer.',
     );
